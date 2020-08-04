4d8a448bb1fb382fe2530c0e7f0f940d58a50c7b
josepereza <josepereza66@gmail.com>
2020-08-04 18:31:05 +0200

    first2 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent } from './components/about/about.component'
+import { HomeComponent } from './components/home/home.component';
+import {HeroesComponent} from './components/heroes/heroes.component'
+import { HeroeComponent } from './components/heroe/heroe.component'
+import { BuscarheroesComponent} from './components/buscarheroes/buscarheroes.component'
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'heroes', component:HeroesComponent},
+  {path:'buscarheroes/:termino', component:BuscarheroesComponent},
+  {path:'heroe/:id', component:HeroeComponent},
+
+  {path:'**',pathMatch: 'full', redirectTo: 'home'},
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscarheroesComponent } from './components/buscarheroes/buscarheroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    BuscarheroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
